cd0be51f0fad0875abd728a7adc9bc2e99616ba7
RileyJaeke <118500890+RileyJaeke@users.noreply.github.com>
2022-11-21 18:29:10 -0600

    Add files via upload

---
+++ area.py
@@ -0,0 +1,11 @@
+import math
+def triangle(b, h):
+    return print(f'Triangle area = {((b*h)/2.0):.2f}')
+def rectangle(l,w):
+    return print(f'Rectangle area = {(w*l*1.0):.2f}')
+def square(b):
+    return print(f'Square area = {math.pow(b,2):.2f}')
+def circle(r):
+    return print(f'Circle area = {(math.pi)*(math.pow(r,2)):.2f}')
+
+
