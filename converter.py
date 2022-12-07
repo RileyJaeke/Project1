edbedb99b6630c377609b6c051ce358fa3d71b49
RileyJaeke <rileyjaeke@gmail.com>
2022-12-06 21:12:20 -0600

    Converts the measurements to another measurement

--- converter.py
+++ converter.py
@@ -1,51 +1,51 @@
 import math as m
 from calculate import *
 
-def inches_to_feet(area):
+def inches_to_feet(area: float) -> float:
     calc = area / 12
     return calc
 
-def inches_to_yards(area):
+def inches_to_yards(area: float) -> float:
     calc = area / 36
     return calc
 
-def inches_to_miles(area):
+def inches_to_miles(area: float) -> float:
     calc =  area / 63360
     return calc
 
-def feet_to_inches(area):
+def feet_to_inches(area: float) -> float:
     calc = area * 12
     return calc
 
-def feet_to_yards(area):
+def feet_to_yards(area: float) -> float:
     calc = area / 3
     return calc
 
-def feet_to_miles(area):
+def feet_to_miles(area: float) -> float:
     calc = area / 5280
     return calc
 
-def yards_to_inches(area):
+def yards_to_inches(area: float) -> float:
     calc = area * 36
     return calc
 
-def yards_to_feet(area):
+def yards_to_feet(area: float) -> float:
     calc = area * 3
     return calc
 
-def yards_to_miles(area):
+def yards_to_miles(area: float) -> float:
     calc = area / 1760
     return calc
 
-def miles_to_inches(area):
+def miles_to_inches(area: float) -> float:
     calc = area * 63360
     return calc
 
-def miles_to_feet(area):
+def miles_to_feet(area: float) -> float:
     calc = area * 5280
     return calc
 
-def miles_to_yards(area):
+def miles_to_yards(area: float) -> float:
     calc = area * 1760
     return calc
 
